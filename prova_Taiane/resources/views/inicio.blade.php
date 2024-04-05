309acf7124d037bd30959abc797eb9446a21fb22
Taiane091 <127451386+Taiane091@users.noreply.github.com>
2024-04-05 23:37:23 +0000

    laravel

--- prova_Taiane/resources/views/inicio.blade.php
+++ prova_Taiane/resources/views/inicio.blade.php
@@ -9,28 +9,28 @@
 <body>
     <p>Olá Mundo!</p>
     <div class="alert alert-primary" role="alert">
-  Um simples alerta primary. Olha só!
+  Um simples alerta primary com <a href="#" class="alert-link">um link de exemplo</a>. Clique nele, se quiser.
 </div>
 <div class="alert alert-secondary" role="alert">
-  Um simples alerta secondary. Olha só!
+  Um simples alerta secondary com <a href="#" class="alert-link">um link de exemplo</a>. Clique nele, se quiser.
 </div>
 <div class="alert alert-success" role="alert">
-  Um simples alerta success. Olha só!
+  Um simples alerta success com <a href="#" class="alert-link">um link de exemplo</a>. Clique nele, se quiser.
 </div>
 <div class="alert alert-danger" role="alert">
-  Um simples alerta danger. Olha só!
+  Um simples alerta danger com <a href="#" class="alert-link">um link de exemplo</a>. Clique nele, se quiser.
 </div>
 <div class="alert alert-warning" role="alert">
-  Um simples alerta warning. Olha só!
+  Um simples alerta warning com <a href="#" class="alert-link">um link de exemplo</a>. Clique nele, se quiser.
 </div>
 <div class="alert alert-info" role="alert">
-  Um simples alerta info. Olha só!
+  Um simples alerta info com <a href="#" class="alert-link">um link de exemplo</a>. Clique nele, se quiser.
 </div>
 <div class="alert alert-light" role="alert">
-  Um simples alerta light. Olha só!
+  Um simples alerta light com <a href="#" class="alert-link">um link de exemplo</a>. Clique nele, se quiser.
 </div>
 <div class="alert alert-dark" role="alert">
-  Um simples alerta dark. Olha só!
+  Um simples alerta dark com <a href="#" class="alert-link">um link de exemplo</a>. Clique nele, se quiser.
 </div>
 </body>
 </html>
